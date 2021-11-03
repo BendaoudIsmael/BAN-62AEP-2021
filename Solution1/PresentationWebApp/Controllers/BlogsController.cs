1e31f10a818eefc754b5e1f12ab3db810cb93714
Ismael Bendaoud <ismael.bendaoud@outlook.com>
2021-11-03 12:29:53 +0000

    Started FileUpload

--- Solution1/PresentationWebApp/Controllers/BlogsController.cs
+++ Solution1/PresentationWebApp/Controllers/BlogsController.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Application.Services;
 using Application.Interfaces;
+using Application.ViewModels;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace PresentationWebApp.Controllers
 {
@@ -12,10 +16,13 @@ namespace PresentationWebApp.Controllers
     {
         private IBlogservice service;
         private ICategoryService categoryService;
-        public BlogsController(IBlogservice _service, ICategoryService _categoryService)
+        private IWebHostEnvironment hostEnviorment;
+
+        public BlogsController(IBlogservice _service, ICategoryService _categoryService, IWebHostEnvironment _hostEnviorment)
             {
             service = _service;
             categoryService = _categoryService;
+            hostEnviorment = _hostEnviorment;
             }
 
 
@@ -36,6 +43,45 @@ namespace PresentationWebApp.Controllers
             var list = categoryService.GetCategories();
             ViewBag.Categories = list;
 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(BlogCreationModel model, IFormFile logoFile)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(model.Name))
+                {
+                    ViewBag.Error = "Name should not be left empty";
+                }
+                else
+                {
+                    if(logoFile != null)
+                    {
+                        //save the file
+
+                        //1. genereate a new UNIQUE filename for the file
+                        string newfilename = Guid.NewGuid() + System.IO.Path.GetExtension(logoFile.FileName); //genereate a serial number which will be unique
+
+                        //2. get the absoulute path of the folder "Files"
+                        string absolutePath = Path.Combine(hostEnviorment.WebRootPath, newfilename); 
+
+                        //3. save the file into the absoulute Path
+                        //FileStream fs = new FileStream("", FileMode.CreateNew, FileAccess.ReadWrite);
+                        //fs.Close();
+                    }
+                    service.AddBlog(model);
+                    ViewBag.Message = "Blog added successfully";
+                }  
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Blog was not added due to an error. try later";
+            }
+
+            var list = categoryService.GetCategories();
+            ViewBag.Categories = list;
             return View();
         }
     }
